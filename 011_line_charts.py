20901664dfc2e794a2c865a0cf99d4d3009cbe27
allenmo <allen02403@gmail.com>
2016-04-12 07:34:22 +0800

    add 011

---
+++ 011_line_charts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [2,3,4,7,9,13,15,17]
+plt.plot(x, 'bo-')
+plt.ylabel('Sunlight')
+plt.xlabel('Time')
+plt.show()
+
+

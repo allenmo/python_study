ae6de73a93d9f1f5660d50b23a46fd607cd7fbbd
allenmo <allen02403@gmail.com>
2016-05-09 13:38:28 +0800

    add 025

---
+++ 025_std_main.py
@@ -0,0 +1,8 @@
+if __name__=='__main__':
+    import sys
+    app=QtGui.QApplication(sys.argv)
+    Form=QtGui.QWidget()
+    ui=Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())

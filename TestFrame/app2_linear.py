153b9f29c3a620bd4cc42a4e44ef73b25499dab4
allenmo <allen02403@gmail.com>
2016-05-16 07:31:18 +0800

    update app2

--- TestFrame/app2_linear.py
+++ TestFrame/app2_linear.py
@@ -31,10 +31,9 @@ class InitTest():
         
     def get_mac_address(self):
         import uuid
-        node = uuid.getnode()
-        mac = uuid.UUID(int = node).hex[-12:]
-        return mac
-
+        mac = uuid.UUID(int =  uuid.getnode()).hex[-12:]
+        return ":".join([mac[e:e+2] for e in range(0,11,2)])
+        #renurn mac
 
         
 if __name__ == '__main__':

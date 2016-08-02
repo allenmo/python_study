51bbb46bed9bdcd52853b2308abfccc8243b41ea
allenmo <allen02403@gmail.com>
2016-08-03 07:33:02 +0800

    upgrade 040

--- 040_argparse.py
+++ 040_argparse.py
@@ -1,4 +1,8 @@
 import argparse
 parser = argparse.ArgumentParser(description="some information here")
+parser.add_argument("echo", help="echo the string you use here")
+parser.add_argument("x", help="int, output x ** 2 ", type=int)
+parser.add_argument("-a", action="store_true")
 args = parser.parse_args()
-
+print args.echo
+print args.x ** 2

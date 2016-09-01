2425f61665e7021cc6be0f0f8ed9383b7234ec26
allenmo <allen02403@gmail.com>
2016-09-01 11:14:51 +0800

    update re 001

--- re/001_re_match.py
+++ re/001_re_match.py
@@ -21,18 +21,18 @@ print r"m.expand(r'\2 \1\3'):", m.expand(r'\2 \1\3')
 
 
 ### output ###
-#m.string: hello world!
-#m.re: <_sre.SRE_Pattern object at 0x7f0f48ef59c0>
-#m.pos: 0
-#m.endpos: 12
-#m.lastindex: 3
-#m.lastgroup: sign
-#m.group(): hello world!
-#m.group(1,2): ('hello', 'world')
-#m.groups(): ('hello', 'world', '!')
-#m.groupdict(): {'sign': '!'}
-#m.start(2): 6
-#m.end(2): 11
-#m.span(2): (6, 11)
-#m.expand(r'\2 \1\3'): world hello!
+# m.string: hello world!
+# m.re: <_sre.SRE_Pattern object at 0x7f0f48ef59c0>
+# m.pos: 0
+# m.endpos: 12
+# m.lastindex: 3
+# m.lastgroup: sign
+# m.group(): hello world!
+# m.group(1,2): ('hello', 'world')
+# m.groups(): ('hello', 'world', '!')
+# m.groupdict(): {'sign': '!'}
+# m.start(2): 6
+# m.end(2): 11
+# m.span(2): (6, 11)
+# m.expand(r'\2 \1\3'): world hello!
 

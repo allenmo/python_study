fda0ef4b9f8ce6fd369359c6e49476b020a949c9
allenmo <allen02403@gmail.com>
2016-09-23 07:29:51 +0800

    update 055

--- 055_glob.py
+++ 055_glob.py
@@ -1,13 +1,17 @@
 import glob
 
-print glob.glob(r"*.jpg")
+print  r'glob.glob(r"*.jpg")', "\n", glob.glob(r"*.jpg"), "\n"
 #output something like: ['abc.jpg', 'aaa.jpg', 'bbb.jpg']
 
-print glob.glob(r'../*.*')
+print r"glob.glob(r'../*.*')", "\n", glob.glob(r'../*.*'), "\n"
 
-print glob.glob(r'/*')
+print r"glob.glob(r'/*')", "\n", glob.glob(r'/*'), "\n"
 
-print glob.glob(r'/home/pi/*')
+print r"glob.glob(r'/home/pi/*')", "\n", glob.glob(r'/home/pi/*'), "\n"
+
+print r"glob.glob(r'mv.jpg*')", "\n", glob.glob(r'mv.jpg*'), '\n'
+
+print r"glob.glob(r'[m-r]*')", "\n", glob.glob(r'[m-r]*'), "\n"
 
 #---------------------------------
 #iglob

c46e77554745d3cce3ccaad703ee5aea56919128
allenmo <allen02403@gmail.com>
2016-09-28 16:33:38 +0800

    add subprocess 002

--- subprocess/001_subprocess.py
+++ subprocess/001_subprocess.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 # pass args in a single string with no space, shell can be True or False
 #subprocess.Popen('ls', shell=True)
@@ -8,6 +9,18 @@ import subprocess
 #subprocess.Popen('ls -l', shell=True)
 # if set sell=False, need to pass args in sequence
 #subprocess.Popen(['ls','-l'], shell=False)
-subprocess.Popen(('ls','-l'), shell=False)
 
+subprocess.Popen(('ls','-la'), shell=False)
 
+print "-------------------------------"
+child = subprocess.Popen(["ping", "www.baidu.com", "-c", "3"])
+# child.wait()
+print "child OBJ:", child
+print "child.pid:", child.pid
+time.sleep(0.5)
+print "child.poll:", child.poll
+# child.kill()
+# child.send_signal(child.SIGTERM)
+child.terminate()
+
+child.wait()

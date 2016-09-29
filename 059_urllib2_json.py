698237bc713e8ba3c976117c1bda0f8782b7cff7
allenmo <allen02403@gmail.com>
2016-09-29 17:34:18 +0800

    update 059

--- 059_urllib2_json.py
+++ 059_urllib2_json.py
@@ -1,13 +1,18 @@
 import urllib, urllib2, json
 
 def http_post():
-    url='http://192.168.1.188/prog_check.php'
+    url='http://192.168.1.88/prog/prog_check.php'
+    #url='http://allstack.net/prog/prog_check.php'
     values = {'user':'Smith', 'passwd':'123456'}
 
-    jdata = json.dumps(values)
+    jdata = json.dumps(values,sort_keys=True, indent=4)
     req = urllib2.Request(url, jdata)
     response = urllib2.urlopen(req)
     return response.read()
 
 resp = http_post()
-print resp
+print resp, "\n"
+jobj = json.loads(resp)
+print jobj, "\n"
+numbers = jobj['numbers']
+print numbers
--- prog_check.php
+++ prog_check.php
@@ -1,5 +1,20 @@
 <?php
-#echo "abc";
-print_r(json_encode($_POST['user']));
+header("Content-Type: text/json");
+#print_r(json_encode($_POST['user']));
 #print_r(json_encode('{"data":"content"}'));
+# $phpInput = file_get_contents("php://input");
+# print_r($phpInput);
+
+$phpInput = file_get_contents("php://input");
+$jobj = json_decode($phpInput, true);
+$username = $jobj['user'];
+
+
+$fruits = array(
+	"fruits" => array("a" => "orange", "b" => "banana", "c" => "apple"),
+	"numbers" => array(1, 2, 3, 4, 5, 6),
+	"holes" => array("first", 5=> "second", "third"),
+	"userBack" => array("allen", $username)
+	);
+echo json_encode($fruits);
 ?>
